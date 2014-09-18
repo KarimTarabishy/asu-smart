6b4f22ddc40d6142b70ef24025141f38f9857099
Karim Tarabishy <tarabishy_2020@hotmail.com>
2014-09-18 10:30:58 +0300

    Adding angularjs template caching in build system.

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,25 @@ var
     util = require('./buildutil.js'),
     livereload = require('gulp-livereload'),
     globule = require("globule"),
-    path = require("path")
+    path = require("path"),
+    templateCache = require("gulp-angular-templatecache");
     ;
 
-gulp.task('js-concat',function(){  // depends on stylus task
-    return gulp.src(['app/app.js','app/components/**/*.js'])
+gulp.task('template-cache', function(){
+    return gulp.src(['app/components/**/*.html'])
+        .pipe(plumber())
+        .pipe(util.count({
+            str: "cached",
+            title: 'template-cache'
+        }))
+        .pipe(templateCache('templates.js',{
+            root: "components/",
+            module: "app"
+        }))
+        .pipe(gulp.dest('build/'));
+});
+function jsConcat(){
+    return gulp.src(['app/app.js','app/components/**/*.js','build/templates.js'])
         .pipe(plumber())
         .pipe(util.count({
             str: "concated",
@@ -21,6 +35,10 @@ gulp.task('js-concat',function(){  // depends on stylus task
         }))
         .pipe(concat('app.js'))
         .pipe(gulp.dest('app/include/'));
+}
+
+gulp.task('js-concat',function(){
+    return jsConcat();
 });
 
 
@@ -56,11 +74,14 @@ gulp.task('smart-clean', function(){
     util.makeSimilar('app','build/css');
 
 });
-gulp.task("start",['smart-clean','js-concat'], function(){
+gulp.task('dummy',['smart-clean','template-cache'], function(){
+    return jsConcat();
+})
+gulp.task("start",['dummy'], function(){
     livereload.listen();
     setTimeout(function(){gulp.start('css-concat')},0);
-    util.watch(['app/'], function (event, path) {
-        if(globule.isMatch('app/include/**')){
+    util.watch(['app/','build/templates.js'], function (event, path) {
+        if(globule.isMatch('app/include/**',path)){
             livereload.changed(path);
         }
         else if(globule.isMatch('app/**/*.styl', path))
@@ -74,13 +95,16 @@ gulp.task("start",['smart-clean','js-concat'], function(){
                 gulp.start('css-concat');
             }
         }
-        else if(globule.isMatch('app/**/*.js', path))
+        else if(globule.isMatch(['app/**/*.js','build/templates.js'], path))
         {
             gulp.start('js-concat');
         }
+        else if(globule.isMatch('app/index.html', path)){
+            livereload.changed(path);
+        }
         else if(globule.isMatch('app/**/*.html', path))
         {
-            livereload.changed(path);
+            gulp.start('template-cache');
         }
 
 
